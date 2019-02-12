06e864e72684d055e7f69e34de15408016b388f3
dangdung <dangdungcntt@gmail.com>
2019-02-12 18:26:51 +0700

    Add date diff in meta

--- resources/views/templates-view/partial/meta.blade.php
+++ resources/views/templates-view/partial/meta.blade.php
@@ -1,3 +1,3 @@
-<meta property="og:title" content="{{ data_get($data, 'title') }}"/>
+<meta property="og:title" content="{{ data_get($data, 'title') }}@if(data_get($data, 'end_time')) ({{ \Illuminate\Support\Carbon::parse(data_get($data, 'end_time'))->diffForHumans(now()) }}) @endif"/>
 <meta property="og:description" content="{{ data_get($data, 'sub_title') }}"/>
 <meta property="og:image" content="{{ data_get($data, 'background_url', 'https://nddcoder.com/wp-content/uploads/2018/09/logo-blue-600x315.jpg') }}"/>
