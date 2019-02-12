ebc4e6cdd3b4e5d3662885a848198f24c8aa94c9
dangdung <dangdungcntt@gmail.com>
2019-02-12 13:30:00 +0700

    Add meta to share on facebook

---
+++ resources/views/templates-view/partial/meta.blade.php
@@ -0,0 +1,3 @@
+<meta property="og:title" content="{{ data_get($data, 'title') }}"/>
+<meta property="og:description" content="{{ data_get($data, 'sub_title') }}"/>
+<meta property="og:image" content="{{ data_get($data, 'background_url', 'https://nddcoder.com/wp-content/uploads/2018/09/logo-blue-600x315.jpg') }}"/>
