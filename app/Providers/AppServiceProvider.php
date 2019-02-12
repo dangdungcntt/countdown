fe71c43d323abe0573b2ecdfc393994539686dd2
dangdung <dangdungcntt@gmail.com>
2019-02-12 12:57:40 +0700

    Add config https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Http\Services\Contracts\TemplateServiceContract;
 use App\Http\Services\TemplateService;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.https')) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -56,6 +56,8 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
+    'https' => env('APP_HTTPS', false),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
